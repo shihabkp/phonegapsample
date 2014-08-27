b8d6a65f38aace2f29ca9c030900b7862ab2a2d3
shihab <shihab.padath@itcinfotech.com>
2014-08-27 15:03:07 +0530

    added

---
+++ www/cordova_plugins.js
@@ -0,0 +1,48 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.splashscreen/www/splashscreen.js",
+        "id": "org.apache.cordova.splashscreen.SplashScreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
+        "id": "org.apache.cordova.dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/android/notification.js",
+        "id": "org.apache.cordova.dialogs.notification_android",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.vibration/www/vibration.js",
+        "id": "org.apache.cordova.vibration.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/uk.co.ilee.socialmessage/www/socialmessage.js",
+        "id": "uk.co.ilee.socialmessage.SocialMessage",
+        "clobbers": [
+            "socialmessage"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "org.apache.cordova.splashscreen": "0.2.7",
+    "org.apache.cordova.dialogs": "0.2.6",
+    "org.apache.cordova.vibration": "0.3.7",
+    "uk.co.ilee.socialmessage": "0.2.5"
+}
+// BOTTOM OF METADATA
+});
